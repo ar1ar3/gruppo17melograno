e81c25ff8bd36d49bcee5310cb6ae3048e845a30
Hp <Hp@192.168.1.10>
2022-11-14 19:41:47 +0100

    Funzione per il caricamento dei dati

---
+++ my_importdata.m
@@ -0,0 +1,12 @@
+%function to import data
+dir();
+for cont=1 : 10
+    if cont<10
+         load(strcat('ad0',num2str(cont),'.mat'))
+         load(strcat('c0',num2str(cont),'.mat'))
+    end
+    if cont==10
+        load(strcat('ad',num2str(cont),'.mat'))
+        load(strcat('c',num2str(cont),'.mat'))
+    end
+end
