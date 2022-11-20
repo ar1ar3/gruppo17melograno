341a9bff2b45f668004ef518dcc840084be19e04
gaechin <unichini01@gmail.com>
2022-11-20 22:29:58 +0100

    my_pwelch
    bo ci sto provando ma ci mette 6 anni a runnare con una matrice

--- my_importdata.m
+++ my_importdata.m
@@ -2,8 +2,8 @@
 dir();
 for cont=1 : 10
     if cont<10
-         load(strcat('ad0',num2str(cont),'.mat'))
-         load(strcat('c0',num2str(cont),'.mat'))
+        load(strcat('ad0',num2str(cont),'.mat'))
+        load(strcat('c0',num2str(cont),'.mat'))
     end
     if cont==10
         load(strcat('ad',num2str(cont),'.mat'))
