419cc6deba97855f7f8b67525c3c9676370c7760
arian <arian@LAPTOP-9CJ5093N>
2022-11-26 10:31:45 +0100

    modificata load con il path per la folder

--- my_importdata.m
+++ my_importdata.m
@@ -2,11 +2,11 @@
 dir();
 for cont=1 : 10
     if cont<10
-        load(strcat('ad0',num2str(cont),'.mat'))
-        load(strcat('c0',num2str(cont),'.mat'))
+        load(strcat('EEG_data/ ','ad0',num2str(cont),'.mat'))
+        load(strcat('EEG_data/ ','c0',num2str(cont),'.mat'))
     end
     if cont==10
-        load(strcat('ad',num2str(cont),'.mat'))
-        load(strcat('c',num2str(cont),'.mat'))
+        load(strcat('EEG_data/ ','ad',num2str(cont),'.mat'))
+        load(strcat('EEG_data/ ','c',num2str(cont),'.mat'))
     end
 end
