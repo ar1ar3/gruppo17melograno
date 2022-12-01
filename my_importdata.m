30002ba49badda4518d0471444292abf0ec47d2f
Hp <Hp@192.168.1.10>
2022-12-01 16:40:25 +0100

    aggiunta di my cell

--- my_importdata.m
+++ my_importdata.m
@@ -15,6 +15,11 @@ for indx = 1: length(fileListC)
    load(fileListC{indx}) 
 end
 
+%utile per my_pwelch
+%sono due array 1x10 in cui in ogni colonna vi è la matrice contenente i
+%dati per quel paziente
+myCell_ad={ad01 ,ad02 ,ad03 ,ad04 ,ad05 ,ad06 ,ad07 ,ad08 ,ad09 ,ad10};
+myCell_c={c01 ,c02 ,c03 ,c04 ,c05 , c06 ,c07 ,c08 ,c09 ,c10};
 
 % for cont=1 : 10
 %     if cont<10
