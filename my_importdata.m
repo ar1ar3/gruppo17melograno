4bbeffd489c9d1594fc115e45564d3bdfd44a0b9
desig <desig@130.251.255.118>
2022-12-01 15:10:02 +0100

    prova creazione cell array nella function my_importdata() perché sì

--- my_importdata.m
+++ my_importdata.m
@@ -1,15 +1,29 @@
-% function my_importdata()
-dir EEG_data ;
+%function to import data
+[statusA, list] = system( 'dir /B /S ad*.mat' );
+resultA = textscan( list, '%s', 'delimiter', '\n' );
+fileListA = resultA{1};
 
+[statusC, list] = system( 'dir /B /S c*.mat' );
+resultC = textscan( list, '%s', 'delimiter', '\n' );
+fileListC = resultC{1};
 
-
-for cont=1 : 10
-    if cont<10
-        load(strcat('EEG_data/ ','ad0',num2str(cont),'.mat'))
-        load(strcat('EEG_data/ ','c0',num2str(cont),'.mat'))
-    end
-    if cont==10
-        load(strcat('EEG_data/ ','ad',num2str(cont),'.mat'))
-        load(strcat('EEG_data/ ','c',num2str(cont),'.mat'))
-    end
+for indx = 1: length(fileListA)
+   load(fileListA{indx}) 
 end
+
+for indx = 1: length(fileListC)
+   load(fileListC{indx}) 
+end
+
+
+% for cont=1 : 10
+%     if cont<10
+%         load(strcat('EEG_data/ ','ad0',num2str(cont),'.mat'))
+%         load(strcat('EEG_data/ ','c0',num2str(cont),'.mat'))
+%     end
+%     if cont==10
+%         load(strcat('EEG_data/ ','ad',num2str(cont),'.mat'))
+%         load(strcat('EEG_data/ ','c',num2str(cont),'.mat'))
+%     end
+% end
+
